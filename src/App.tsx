2349cd8f181a2e5dbca3596efde661a6a0c0cebe
Gezellligheid <elias.debaveye@student.howest.be>
2022-01-17 22:51:54 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./assets/tailwind/tailwind.css"
+
+function App() {
+	return <div>boe</div>
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	content: ["./src/**/*.{ts, tsx}", "./public/**/*.{ts, tsx,html}"],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+}
